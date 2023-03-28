b3b51a6caad7f34aec2ef809eed729f24dd74d09
Gabriel Soares <gabrsoares@outlook.com>
2023-03-28 15:33:40 -0300

    atvd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/Header/index';
+import IMC from './components/IMC';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Testando" />
+      <IMC />
+    </div>
+  );
+}
+
+export default App;
